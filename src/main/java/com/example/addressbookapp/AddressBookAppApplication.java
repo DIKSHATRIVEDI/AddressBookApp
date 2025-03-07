df473ee8f230c4e7cf2bfd0ec147f05b7085e9c3
DIKSHATRIVEDI <diksha.trivedi2122@gmail.com>
2025-03-07 16:48:12 +0530

    Slf4j Logging added

--- src/main/java/com/example/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/example/addressbookapp/AddressBookAppApplication.java
@@ -1,13 +1,20 @@
 package com.example.addressbookapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookAppApplication.class, args);
+		ApplicationContext context = SpringApplication.run(AddressBookAppApplication.class, args);
+		String activeProfile = context.getEnvironment().getActiveProfiles().length > 0
+				? context.getEnvironment().getActiveProfiles()[0]
+				: "default";
+		log.info("Employee Payroll App Started in {} environment", activeProfile);
 	}
 
 }
