1ab58768bd646d4c2ffdf1555bc15d062430be0d
Diksha <diksha.trivedi2122@gmail.com>
2025-03-10 18:44:13 +0530

    Exception Handling using Try and catch refactored

--- src/main/java/com/example/addressbookapp/Exception/AddressBookException.java
+++
@@ -1,7 +0,0 @@
-package com.example.addressbookapp.Exception;
-
-public class AddressBookException extends RuntimeException {
-    public AddressBookException(String message) {
-        super(message);
-    }
-}
